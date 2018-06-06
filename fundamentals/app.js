c307429aa427caafb2058e634948b855c49a8775
Nattanon Bunyatipanon <nattanon33@yahoo.com>
2018-06-05 20:29:33 -0700

    add git ignore

--- fundamentals/app.js
+++ fundamentals/app.js
@@ -3,12 +3,16 @@ console.log("starting app");
 const fs = require('fs');
 const os = require('os');
 const notes = require('./notes.js');
+const _ = require('lodash')
 
 // Grabs user info
 var user = os.userInfo();
 
 console.log(notes.addNote());
 console.log(notes.add(5, 15));
+console.log(_.isString(true));
+console.log(_.isString('n'));
+console.log(_.uniq(['Non', 'Non', 1, 2, 3, 1, 2, 3]));
 
 // Appends text to end of file;
 // fs.appendFile('greetings.txt', `\nHello ${user.username}! You are ${notes.age}.\n`, (err) => {
