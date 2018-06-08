11fd1d729181e34a7ac14a7d457c36b6fc5b8fe3
Nattanon Bunyatipanon <nattanon33@yahoo.com>
2018-06-07 18:49:09 -0700

    fundamentals

---
+++ fundamentals/playground/json.js
@@ -0,0 +1,34 @@
+
+
+// Json to String
+
+// var obj = {
+//     name: 'Non'
+// };
+// var stringObj = JSON.stringify(obj);
+// console.log(typeof stringObj);
+// console.log(stringObj);
+
+// String to Json
+
+// var personString = '{"Name": "Non", "Age":21}';
+// var person = JSON.parse(personString);
+// console.log(typeof person);
+// console.log(person);
+const fs = require('fs');
+
+
+var orginalNote = {
+    title: 'Some Title',
+    body: 'Somebody'
+}
+
+// OrginalNoteString writes note into file
+var orginalNoteString = JSON.stringify(orginalNote);
+fs.writeFileSync('notes.json', orginalNoteString)
+
+// Reading note from file
+var noteString = fs.readFileSync('notes.json');
+var note = JSON.parse(noteString);
+console.log(typeof note);
+console.log(note);
