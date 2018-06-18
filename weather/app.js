2e14fda8851124156ffc5898271079b2bcb3245d
Nattanon Bunyatipanon <nattanon33@yahoo.com>
2018-06-17 20:53:10 -0700

    weather func

--- weather/app.js
+++ weather/app.js
@@ -4,5 +4,5 @@ request({
     url: 'https://maps.googleapis.com/maps/api/geocode/json?address=4040%207th%20AVE%20NE%20Seattle,%20WA',
     json: true
 }, (error, response, body) => {
-    console.log(body);
+    console.log(JSON.stringify(body, undefined, 2));
 })
