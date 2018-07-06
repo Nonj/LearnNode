9806d1ff14b3cfa60b0b8eaefd313631d2e92534
Nattanon Bunyatipanon <nattanon33@yahoo.com>
2018-07-05 21:27:52 -0700

    using expect library

--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -1,15 +1,46 @@
 const utils = require('./utils')
+const expect = require('expect')
 
 it('Should add two numbers', ()=> {
     let res = utils.add(33,11);
-    if (res !== 44) {
-        throw new Error(`Value not correct, got ${res}.`);
-    }
+
+    expect(res).toBe(44).toBeA('number');
+    // if (res !== 44) {
+    //     throw new Error(`Value not correct, got ${res}.`);
+    // }
 });
 
 it('Should return the squared number.', () => {
-    let res = utils.square(11)
-    if (res !== 121) {
-        throw new Error(`Wrong value: ${res}`)
-    }
+    let res = utils.square(11);
+
+    expect(res).toBe(121).toBeA('number');
+
+    // if (res !== 121) {
+    //     throw new Error(`Wrong value: ${res}`)
+    // }
 })
+
+it('Should expect some values', ()=> {
+    //expect(12).toNotBe(12);
+    //expect({name:'on'}).toNotEqual({name:'Non'});
+    expect([2,3,4]).toInclude(2);
+    expect([2,3,4]).toExclude(5);
+    expect({
+        name: "non",
+        age:23,
+        location: "seattle"
+    }).toExclude({
+        age:25
+    })
+})
+// Should verify first and last names are set
+// Asserts it includes firstname and lastname with proper values
+it('Should set proper values for firstname and last name', () => {
+    let user = {age: 23, location: 'seattle'}
+    let res = utils.setName(user, "Non Bun");
+
+    expect(res).toInclude({
+        firstname: "Non",
+        lastname: "Bun"
+    });
+});
